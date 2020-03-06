50078fcd23023739a87ade608ae4f8cb77531e0f
Petr Janoš <koksa.janos@gmail.com>
2020-03-06 20:56:47 +0100

    prototyp_1
    funkční kontra programu na přejmenování

--- venv/test.py
+++ venv/test.py
@@ -1,30 +1,27 @@
 import csv
 import os
+from pathlib import Path
 
-inputFileName = "test_data.csv"
-outputFileName = os.path.splitext(inputFileName)[0] + "_modified.csv"
+# cesta k souborům
+data_folder = Path("data/")
 
+# proměnné "input" a "output" souborů
+inputFileName = data_folder / "test_data.csv"
+outputFileName = os.path.splitext(inputFileName)[0] + "_completed.csv"
+
+# nastavení parametrů "inputu" a "outputu"
 with open(inputFileName, newline='', encoding='utf-8-sig') as inFile, open(outputFileName, 'w', newline='', encoding='utf-8-sig') as outfile:
     r = csv.reader(inFile, delimiter=";")
     w = csv.writer(outfile, delimiter=";")
 
-    # získá první řádek
+    # získá první řádek, jestli je "hodnota1", nahraď "hodnota2"
     row1 = next(r)
-    # jestli je "hodnota1", nahraď "hodnota2"
     row1 = [item.replace("Věk", "XXX") for item in row1]
     row1 = [item.replace("Jméno", "YYY") for item in row1]
-    print(row1)
+
+    # vypíše hlavičku a data
+    print("hlavička:", row1)
+    w.writerow(row1)
     for row in r:
-        print(row)  # nahradit w.writerow
-
-
-
-
-    # # přeskočí/přepíše původní hlavičku
-    # next(r, None)
-    # # napíše novou hlavičku
-    # w.writerow(['Item Number', 'Item Description', 'List Price', 'QTY Available'])
-    #
-    # # zkopíruje zbytek
-    # for row in r:
-    #     w.writerow(row)
+        print("data:", row)
+        w.writerow(row)
